cef000078e0d0b11e00467fb4eb2c6e4bc1949c3
Sagar <04017711721_iot@vips.edu>
2023-08-09 20:00:01 +0530

    Button passes with the fn

--- src/pages/BmiPage.jsx
+++ src/pages/BmiPage.jsx
@@ -14,12 +14,18 @@ const BmiPage = () => {
     const HandleHeight = (data) =>{
         //  1. For  handle the height 
         height = data;
+        if(height <=0 ){
+            height = 0;
+        }
         console.log("Height = " , height )
     }
     
     const HandleWeight = (data) =>{
         //  2. For Handle the weight 
         weight = data;
+        if(weight <=0 ){
+            weight = 0;
+        }
         console.log("weight = " , weight )
         
     }
@@ -27,11 +33,20 @@ const BmiPage = () => {
     const HandleAge = (data) =>{
         //  3. For Handle the Age 
         Age = data;
+        if(Age <=0 ){
+            Age = 0;
+        }
+        else if(Age > 120 ){
+            Age = 120;
+        }
         console.log("Age = " , Age )
         
     }
 
     // Now design the Function to calculate the Bmi of the person 
+    const BmiCalculate = ()=>{
+        console.log("I am BmiCalculate")
+    }
 
 return (
     <div className='container row '>
@@ -40,8 +55,8 @@ return (
             <p>Height : <TextFeild fn={HandleHeight}  place ="Enter Height in CM " /> </p>
             <p>Weight : <TextFeild fn={HandleWeight} place ="Enter Weight in KG " /> </p>
             <div className='output row col-1' >
-                <Button  value="Calculate " />
-                <Button  value = "Clear" />
+                <Button fn={BmiCalculate}  value="Calculate " />
+                <Button value = "Clear" />
             </div>
     </div>
 )
